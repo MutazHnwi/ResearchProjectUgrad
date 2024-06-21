a92a633a0245a2e6f00f6f3b7390afe37757f624
Blockinator1 <70295628+Blockinator1@users.noreply.github.com>
2024-06-21 15:13:08 -0600

    Redid randomization function again

--- test.js
+++ test.js
@@ -190,92 +190,60 @@ function isSubMovement() {
     }
 }
 
+// randomizes checkpoints[phase] and checkpoints[phase + 1]
 function placeNewPoints() {
-	// randomize checkpoints[phase] and checkpoints[phase + 1]
-	/* Pseudocode
-	 *
-	 * Find the closest edge of the screen (left, right, top, bottom) to checkpoints[phase - 1]
-	 * Generate an angle (-45-45 degrees offset the angle opposite the closest edge) to place the center between the two
-	 * Generate an angle offset (0-45 degrees)
-	 * Generate a random distance (90px (10px buffer) to max(window.innerWidth, window.innerHeight) 
-	 */
+
+
+
+
+
+	let currentRect = checkpoints[phase - 1].getBoundingClientRect();
+	let rectA = new DOMRect(0, 0, 80, 80);
+	let rectB = new DOMRect(0, 0, 80, 80);
 	
-	// Find the closest edge
-	let currentPoint = checkpoints[phase - 1].getBoundingClientRect();
-	let angle;
-	let distFromEdge;
-	if (
-		currentPoint.x + 40 <= window.innerWidth - currentPoint.x - 40 && // dist from left < dist from right
-		currentPoint.x + 40 <= currentPoint.y + 40 && // dist from left < dist from top
-		currentPoint.x + 40 <= window.innerHeight - currentPoint.y - 40 // dist from left < dist from bottom
-	) { // left
-		angle = 0; // 0 radians points right
-		distFromEdge = currentPoint.x;
-	} else if (
-		window.innerWidth - currentPoint.x - 40 <= currentPoint.x + 40 && // dist from right < dist from left
-		window.innerWidth - currentPoint.x - 40 <= currentPoint.y + 40 && // dist from right < dist from top
-		window.innerWidth - currentPoint.x - 40 <= window.innerHeight - currentPoint.y - 40 // dist from right < dist from bottom
-	) { // right
-		angle = Math.PI; // pi radians points left
-		distFromEdge = window.innerWidth - currentPoint.x - 80;
-	} else if (
-		currentPoint.y + 40 <= window.innerHeight - currentPoint.y - 40 && // dist from top < dist from bottom
-		currentPoint.y + 40 <= currentPoint.x + 40 && // dist from top < dist from left
-		currentPoint.y + 40 <= window.innerWidth - currentPoint.x - 40 // dist from top < dist from right
-	) { // top
-		angle = Math.PI * (3/2); // 3pi/2 radians points down
-		distFromEdge = currentPoint.y;
-	} else if (
-		window.innerHeight - currentPoint.y - 40 <= currentPoint.y + 40 && // dist from bottom < dist from top
-		window.innerHeight - currentPoint.y - 40 <= currentPoint.x + 40 && // dist from bottom < dist from left
-		window.innerHeight - currentPoint.y - 40 <= window.innerWidth - currentPoint.x - 40 // dist from bottom < dist from right
-	) { // bottom
-		angle = Math.PI * (1/2); // pi/2 radians points down
-		distFromEdge = window.innerHeight - currentPoint.y - 80;
-	} else {
-		throw new Error("angle determining error in placeNewPoints");
+	if (phase == 1) { // startpoint
+		rectA.x = 10 + Math.random() * (window.innerWidth / 2 - 100);
+		rectA.y = 10 + Math.random() * (window.innerHeight / 2 - 100);
+		rectB.x = window.innerWidth / 2 + 10 + Math.random() * (window.innerWidth / 2 - 100);
+		rectB.y = 10 + Math.random() * (window.innerHeight / 2 - 100);
+	} else if (currentRect.x < window.innerWidth / 2 && currentRect.y < window.innerHeight / 2) { // top left quadrant
+		rectA.x = window.innerWidth / 2 + Math.random() * (window.innerWidth / 2 - 100);
+		rectA.y = 10 + Math.random() * (window.innerHeight / 2 - 100);
+		rectB.x = 10 + Math.random() * (window.innerWidth - 100);
+		rectB.y = window.innerHeight / 2 + 10 + Math.random() * (window.innerHeight / 2 - 100);
+	} else if (currentRect.x > window.innerWidth / 2 && currentRect.y < window.innerHeight / 2) { // top right
+		rectA.x = 10 + Math.random() * (window.innerWidth / 2 - 100);
+		rectA.y = 10 + Math.random() * (window.innerHeight / 2 - 100);
+		rectB.x = 10 + Math.random() * (window.innerWidth - 100);
+		rectB.y = window.innerHeight / 2 + 10 + Math.random() * (window.innerHeight / 2 - 100);
+	} else if (currentRect.x < window.innerWidth / 2 && currentRect.y > window.innerHeight / 2) { // bottom left
+		rectA.x = window.innerWidth / 2 + Math.random() * (window.innerWidth / 2 - 100);
+		rectA.y = window.innerHeight / 2 + 10 + Math.random() * (window.innerHeight / 2 - 100);
+		rectB.x = 10 + Math.random() * (window.innerWidth - 100);
+		rectB.y = 10 + Math.random() * (window.innerHeight / 2 - 100);
+	} else if (currentRect.x > window.innerWidth / 2 && currentRect.y > window.innerHeight / 2) { // bottom right
+		rectA.x = 10 + Math.random() * (window.innerWidth / 2 - 100);
+		rectA.y = window.innerHeight / 2 + 10 + Math.random() * (window.innerHeight / 2 - 100);
+		rectB.x = 10 + Math.random() * (window.innerWidth - 100);
+		rectB.y = 10 + Math.random() * (window.innerHeight / 2 - 100);
+	} else { // something went wrong
+		throw new Error('cannot find current point');
 	}
-	angle += Math.random() * Math.PI/2 - Math.PI/4;
-	let angleOffset = Math.random() * Math.PI/6 + Math.PI/12;
-	let distA;
-	let RectA = new DOMRect(0, 0, 80, 80);
-	let distB;
-	let RectB = new DOMRect(0, 0, 80, 80);
-	while (RectA.x < 10 || RectA.x + 90 > window.innerWidth || RectA.y < 10 || RectA.y + 90 > window.innerHeight) { // while RectA is out of bounds
-		distA = Math.random() * (Math.max(window.innerWidth, window.innerHeight) - 90 - distFromEdge) + 90;
-		RectA.x = Math.cos(angle + angleOffset) * distA + currentPoint.x;
-		RectA.y = currentPoint.y - Math.sin(angle + angleOffset) * distA; // y value is 0 at top and window.innerHeight at bottom, so going up means subtracting from y
-	}
-	while ( // while RectB is out of bounds or too close to RectA
-		RectB.x < 10 || // too close to left
-		RectB.x + 90 > window.innerWidth || // too close to right
-		RectB.y < 10 || // too close to top
-		RectB.y + 90 > window.innerHeight || // too close to bottom
-		(
-		!(RectB.x > RectA.x + 90 || RectB.x < RectA.x - 10) && // too close to RectA by X
-		!(RectB.y > RectA.y + 90 || RectB.y < RectA.y - 10) // too close to RectA by Y
-		) || (
-		!(RectB.x > currentPoint.x + 90 || RectB.x < currentPoint.x - 10) && // too close to currentPoint by X
-		!(RectB.y > currentPoint.y + 90 || RectB.y < currentPoint.y - 10) // too close to currentPoint by Y
-		)
-	) {
-		distB = Math.random() * (Math.max(window.innerWidth, window.innerHeight) - 90 - distFromEdge) + 90;
-		RectB.x = Math.cos(angle - angleOffset) * distB + currentPoint.x;
-		RectB.y = currentPoint.y - Math.sin(angle - angleOffset) * distB;
-	}
+	
+
 	if (Math.random() > 0.5) { // Coin flip whether A or B is next
-		checkpoints[phase].style.left = `${RectA.x}px`;
-		checkpoints[phase].style.top = `${RectA.y}px`;
+		checkpoints[phase].style.left = `${rectA.x}px`;
+		checkpoints[phase].style.top = `${rectA.y}px`;
 		checkpoints[phase].style.display = 'flex';
-		checkpoints[phase + 1].style.left = `${RectB.x}px`;
-		checkpoints[phase + 1].style.top = `${RectB.y}px`;
+		checkpoints[phase + 1].style.left = `${rectB.x}px`;
+		checkpoints[phase + 1].style.top = `${rectB.y}px`;
 		checkpoints[phase + 1].style.display = 'flex';
 	} else {
-		checkpoints[phase].style.left = `${RectB.x}px`;
-		checkpoints[phase].style.top = `${RectB.y}px`;
+		checkpoints[phase].style.left = `${rectB.x}px`;
+		checkpoints[phase].style.top = `${rectB.y}px`;
 		checkpoints[phase].style.display = 'flex';
-		checkpoints[phase + 1].style.left = `${RectA.x}px`;
-		checkpoints[phase + 1].style.top = `${RectA.y}px`;
+		checkpoints[phase + 1].style.left = `${rectA.x}px`;
+		checkpoints[phase + 1].style.top = `${rectA.y}px`;
 		checkpoints[phase + 1].style.display = 'flex';
 	}
 }
