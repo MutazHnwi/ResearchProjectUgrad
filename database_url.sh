d2c8caf5e4859acbda0c7ccd342c08c8f13f14a7
Sujit Kumar <sujitkumar20501@gmail.com>
2024-08-12 00:30:21 -0600

    added procfile

--- database_url.sh
+++
@@ -1,1 +0,0 @@
-export DATABASE_URL="postgres://uc4trs5h37bn68:pb5429d63f8822e4a4a2624670ebdf28e141853bc8042f2e57c759e430d851d77@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d1eu3llgsa602"
